0f1f7b40add1409192990d833aa3ffbe383bc463
virza805 <virza805@gmail.com>
2019-08-16 21:47:54 +0600

    whil lope and series

--- while.js
+++ while.js
@@ -1,43 +1,43 @@
-// var factorial = 1;
-// for (var i=1; i<=10; i++){
-//     factorial *= i;
-//     console.log("Factorial of",i,"is",factorial);
-// } 
+var factorial = 1;
+for (var i=1; i<=10; i++){
+    factorial *= i;
+    console.log("Factorial of",i,"is",factorial);
+} 
 
-// console.log("\n");
+console.log("\n");
 
-// var series = "";
-// var n;
-// for(var i=1;i<10;i++){
-//     n = i*2;
-//     series = series + n + "+";
-// }
-// console.log(series);
+var series = "";
+var n;
+for(var i=1;i<10;i++){
+    n = i*2;
+    series = series + n + "+";
+}
+console.log(series);
 
-// series ="";
-// for(var i=1;i<10;i++){
-//     n = i;
-//     if( n%2==0){
-//         n = i*2;
-//     }
-//     series = series + n + "+";
-// }
-// console.log("Series 4 = ",series);
-// // fibonacci Seres 
-// //  0 1 1 2 3 5 8 13 21 34 55 
+series ="";
+for(var i=1;i<10;i++){
+    n = i;
+    if( n%2==0){
+        n = i*2;
+    }
+    series = series + n + "+";
+}
+console.log("Series 4 = ",series);
+// fibonacci Seres 
+//  0 1 1 2 3 5 8 13 21 34 55 
 
-// series = "0 1 ";
-// n = 0;
-// var x = 0;
-// var y = 1;
+series = "0 1 ";
+n = 0;
+var x = 0;
+var y = 1;
 
-// for(var i = 1; i<10; i++){
-//     n= x+y;
-//     x=y;
-//     y=n;
-//     series = series + n + " ";
-// }
-// console.log("fibonacci Series = ",series);
+for(var i = 1; i<10; i++){
+    n= x+y;
+    x=y;
+    y=n;
+    series = series + n + " ";
+}
+console.log("fibonacci Series = ",series);
 
 // 1 to 20 গুণের নামতা
 var series;

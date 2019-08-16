bf99c2c064cc3815dcce56df37beae2490c09e77
virza805 <virza805@gmail.com>
2019-08-16 21:29:27 +0600

    JavaScript

---
+++ while.js
@@ -0,0 +1,46 @@
+// var factorial = 1;
+// for (var i=1; i<=10; i++){
+//     factorial *= i;
+//     console.log("Factorial of",i,"is",factorial);
+// } 
+
+// console.log("\n");
+
+// var series = "";
+// var n;
+// for(var i=1;i<10;i++){
+//     n = i*2;
+//     series = series + n + "+";
+// }
+// console.log(series);
+
+// series ="";
+// for(var i=1;i<10;i++){
+//     n = i;
+//     if( n%2==0){
+//         n = i*2;
+//     }
+//     series = series + n + "+";
+// }
+// console.log("Series 4 = ",series);
+// // fibonacci Seres 
+// //  0 1 1 2 3 5 8 13 21 34 55 
+
+// series = "0 1 ";
+// n = 0;
+// var x = 0;
+// var y = 1;
+
+// for(var i = 1; i<10; i++){
+//     n= x+y;
+//     x=y;
+//     y=n;
+//     series = series + n + " ";
+// }
+// console.log("fibonacci Series = ",series);
+
+var series;
+for(var i =0; i<10; i++){
+    series = i;
+    
+}
